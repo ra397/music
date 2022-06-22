8dc6ce1dc8f1b739ed3e27e1dca7ec0a7a55a587
Rabi Alaya <rabialaya@gmail.com>
2022-06-21 23:46:21 -0500

    udpate

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 from app import routes
