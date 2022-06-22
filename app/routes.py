4e5a5826ba1c0c108695941bdf5a9cf19ad87cd6
Rabi Alaya <rabialaya@gmail.com>
2022-06-22 00:05:33 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from click import password_option
 from flask import render_template, request, session, redirect, url_for
 from app import app, db
 from app.models import User
@@ -6,19 +7,29 @@ from app.models import User
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # get data from html elements
+        # get data from form
         username = request.form['username']
         password = request.form['password']
         
         # authenticate user
         
-        
+
         return redirect(url_for('login'))
 
     return render_template('login.html')
 
 @app.route("/register")
 def register():
+    if request.method == 'POST':
+        # get data from form
+        username = request.form['username']
+        password_option = request.form['password']
+
+        # check username is unique
+
+        # add to db
+
+
     return render_template('register.html')
 
 @app.route("/main")
