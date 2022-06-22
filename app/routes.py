8c4f8f1a0326ab5317471fb69674d78055475510
Rabi Alaya <rabialaya@gmail.com>
2022-06-22 15:26:18 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
+from crypt import methods
 from app import app
-from flask import render_template
+from flask import render_template, request
 
-@app.route('/login')
+@app.route('/login', methods=['Get', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/login_validation', methods=['Get', 'POST'])
+def login_validation():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return '{} \n {}'.format(username, password)
+
+@app.route('/register', methods=['Get', 'POST'])
 def register():
     return render_template('register.html')
 
