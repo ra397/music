f5501f2b9789aff76ea7c8408c907549c043e89b
Rabi Alaya <rabialaya@gmail.com>
2022-06-30 17:52:38 -0500

    working on popup iframe

---
+++ app/static/popup.js
@@ -0,0 +1,7 @@
+function popup() {
+    let popup = document.getElementById('popup')
+    popup.style.display = 'block'
+    
+    const iframe = document.createElement('iframe')
+    iframe.setAttribute('src', 'https://youtube.com');
+}
