d0c66e18ad7f2e05535eccc40f5aa780bc56297b
Rabi Alaya <rabialaya@gmail.com>
2022-08-12 12:42:07 -0500

    added exit player button

---
+++ app/static/exit.js
@@ -0,0 +1,12 @@
+const exit = document.getElementById('exit')
+exit.style.display = 'none'
+
+function showExit() {
+    const exit = document.getElementById('exit')
+    exit.style.display = 'inline'
+}
+
+function hideExit() {
+    const exit = document.getElementById('exit')
+    exit.style.display = 'none'
+}
